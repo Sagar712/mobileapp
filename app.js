c77138a97f978820e55bcec2547043247319cea1
Sagar712 <74832706+Sagar712@users.noreply.github.com>
2021-02-17 23:03:28 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,65 @@
+function swipeleft1(){
+    document.getElementById("content1").style.transform="translateX(0%)";
+    document.getElementById("content2").style.transform="translateX(100%)";
+    document.getElementById("content3").style.transform="translateX(200%)";
+    document.getElementById("icon1").style.color="#fff";
+    document.getElementById("icon2").style.color="rgb(151, 202, 209)";
+    document.getElementById("icon3").style.color="rgb(151, 202, 209)";
+
+}
+
+function swipeleft2(){
+    document.getElementById("content1").style.transform="translateX(-100%)";
+    document.getElementById("content2").style.transform="translateX(0%)";
+    document.getElementById("content3").style.transform="translateX(100%)";
+    document.getElementById("icon2").style.color="#fff";
+    document.getElementById("icon1").style.color="rgb(151, 202, 209)";
+    document.getElementById("icon3").style.color="rgb(151, 202, 209)";
+}
+
+function swipeleft3(){
+    document.getElementById("content1").style.transform="translateX(-200%)";
+    document.getElementById("content2").style.transform="translateX(-100%)";
+    document.getElementById("content3").style.transform="translateX(0%)";
+    document.getElementById("icon3").style.color="#fff";
+    document.getElementById("icon2").style.color="rgb(151, 202, 209)";
+    document.getElementById("icon1").style.color="rgb(151, 202, 209)";
+}
+
+let chngeicn=0;
+const overlay = document.querySelector(".ovelay2");
+function changeicon(){
+    chngeicn++;
+    const drop = document.querySelector(".dropdown");
+    if(chngeicn%2!=0){
+        document.getElementById("changeiconq").className="fas fa-times";
+        drop.classList.toggle("active");
+        overlay.classList.toggle("active");
+    }
+        
+    else{
+        document.getElementById("changeiconq").className="fas fa-bars";
+        drop.classList.toggle("active");
+        overlay.classList.toggle("active");
+    }
+        
+}
+
+
+
+function overlayhandle(){
+    console.log("overlay clicked");
+    chngeicn++;
+    const drop = document.querySelector(".dropdown");
+    if(chngeicn%2!=0){
+        document.getElementById("changeiconq").className="fas fa-times";
+        drop.classList.toggle("active");
+        overlay.classList.toggle("active");
+    }
+        
+    else{
+        document.getElementById("changeiconq").className="fas fa-bars";
+        drop.classList.toggle("active");
+        overlay.classList.toggle("active");
+    }
+}
